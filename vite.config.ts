84e30d91a51b919ee061e30dd8f393306b94141d
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-05-27 16:19:20 +0300

    chore: add X-Content-Type-Options + tsconfig moduleResolution PascalCase

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     port: Number(process.env.PORT) || 3000,
     headers: {
       'X-Frame-Options': 'DENY',
+      'X-Content-Type-Options': 'nosniff',
       'Content-Security-Policy':
         "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline';",
       'Referrer-Policy': 'strict-origin-when-cross-origin',
